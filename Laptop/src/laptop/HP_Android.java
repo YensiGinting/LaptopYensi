df26382fc57dda37f48fff30279977902a2a703c
YensiGinting <96478638+YensiGinting@users.noreply.github.com>
2022-11-22 14:08:28 +0700

    Add files via upload

---
+++ Laptop/src/laptop/HP_Android.java
@@ -0,0 +1,23 @@
+package HP_Android;
+
+public class HP_Android {
+String Merk;
+String Warna;
+String Jenisprocessor;
+String UkuranLayar;
+
+    public static void main(String[] args) {
+        Laptop laptopku= new Laptop ();
+        laptopku.Merk="Lenovo";
+        laptopku.Warna="pink";
+        laptopku.Jenisprocessor="AMD";
+        laptopku.UkuranLayar="17 inchi";
+        
+        System.out.println("Merk           :"+ laptopku.Merk);
+        System.out.println("Warna          :"+ laptopku.Warna);
+        System.out.println("Jenis Processor:"+ laptopku.Jenisprocessor);
+        System.out.println("Ukuran Layar   :"+ laptopku.UkuranLayar);
+
+    }
+    
+}
